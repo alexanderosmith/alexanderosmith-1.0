5bcf28000bf1fe6376e8eb7bdeed472769ba6759
Alexander O. Smith <aos11409@gmail.com>
2018-03-03 18:57:54 -0500

    Blog Update
    Blog Update

---
+++ images/computermediation_files/radar.js
@@ -0,0 +1,5 @@
+/* Radar.js v20.46. Copyright 2017 Cedexis. All rights reserved. */
+if(!cedexis||!cedexis.start){if(!cedexis)var cedexis={};(function(_){
+var g=function(c){var d=e;c=c.split(".");for(var a=f,b;c.length&&(b=c.shift());)c.length||void 0===d?a[b]&&a[b]!==Object.prototype[b]?a=a[b]:a=a[b]={}:a[b]=d},e=function(){},f=this;g("cedexis.start");g("cedexis.impact");
+})(cedexis)}cedexis.start();
+
